86c6be3f36a294479bd36535de8f3a295c40655f
walt_to_art <guard.mauricio.save@gmail.com>
2026-09-10 18:16:58 -0500

    feat(theme): initialize Waltiva theme system

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import AddModules from './modules/index'
+import { initWaltivaThemeSystem } from './utils/waltivaTheme'
+
+initWaltivaThemeSystem()
 
 const app = createApp(App)
 AddModules({ app, router })
