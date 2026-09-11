f88b245a41a04f61a27635a0bc3c302ae6390dda
walt_to_art <guard.mauricio.save@gmail.com>
2026-09-10 21:03:02 -0500

    Activar compatibilidad de SmartArt y dropdown de fuentes

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import App from './App.vue'
 import router from './router'
 import AddModules from './modules/index'
 import { initWaltivaThemeSystem } from './utils/waltivaTheme'
+import { initSmartArtCompatibilityFix } from './utils/smartArtFix'
 
 initWaltivaThemeSystem()
+initSmartArtCompatibilityFix()
 
 const app = createApp(App)
 AddModules({ app, router })
